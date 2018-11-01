6752e1824f5aa8326b554386a3fd8af94efd914e
KleberDevMen <klebersubcontas@gmail.com>
2018-11-01 10:20:31 -0300

    .

---
+++ db/migrate/20181031150039_add_invoice_to_answers.rb
@@ -0,0 +1,5 @@
+class AddInvoiceToAnswers < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :answers, :invoice, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20181031120123) do
-
+ActiveRecord::Schema.define(version: 20181031150039) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "desc_answer"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "invoice_id"
+    t.index ["invoice_id"], name: "index_answers_on_invoice_id"
   end
-
 
   create_table "invoices", force: :cascade do |t|
     t.string   "cnpj"
@@ -34,7 +33,6 @@ ActiveRecord::Schema.define(version: 20181031120123) do
     t.string   "name_addressee"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-
   end
 
   create_table "questions", force: :cascade do |t|
@@ -45,7 +43,6 @@ ActiveRecord::Schema.define(version: 20181031120123) do
     t.boolean  "q1"
     t.boolean  "q2"
     t.boolean  "q3"
-
   end
 
 end
